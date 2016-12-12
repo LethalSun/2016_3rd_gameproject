c7260fb1d9ab03bb9665ed6281296296cb11829e
highfence <highfence@naver.com>
2016-12-12 10:28:50 +0900

    Update for merge

--- IDCA/Classes/Enemy.cpp
+++ IDCA/Classes/Enemy.cpp
@@ -83,7 +83,7 @@ void Enemy::MoveEnemy(const float deltaTime)
 {
 	auto position = m_pManageEnemyMove->update(this->getPosition(), getTranslatedUnitVec(), getMapPointer(), deltaTime, this);
 
-	//this->setPosition(position);
+	this->setPosition(position);
 	return;
 }
 
