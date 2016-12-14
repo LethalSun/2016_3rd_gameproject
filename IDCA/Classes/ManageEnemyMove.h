35b1341e60035aa08c4fe29d55d7cbb951c788aa
PongKak <qkfkathr42@naver.com>
2016-12-14 11:36:20 +0900

    오류 고침

--- IDCA/Classes/ManageEnemyMove.h
+++ IDCA/Classes/ManageEnemyMove.h
@@ -3,19 +3,19 @@ class ManageMap;
 class Enemy;
 class EnemyManager;
 
+
+
 class ManageEnemyMove : public Component
 {
 public:
 
 	CREATE_FUNC(ManageEnemyMove);
-	ManageEnemyMove() = default;
-	~ManageEnemyMove() = default;
 
 
 
-	bool init();
+	virtual bool init() override;
 
-	void SetEnemyManager(EnemyManager * ptr);
+	//void SetEnemyManager(EnemyManager * ptr);
 
 	Vec2 update(Vec2 position, Vec2 unitvec, TMXTiledMap * map, const float,Enemy* );
 	Vector<Enemy*>* enemyVector;
