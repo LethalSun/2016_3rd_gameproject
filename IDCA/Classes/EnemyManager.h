5f487e5ee840d7282070128f056b3722d594d38e
Lee Kwenwon <highfence@naver.com>
2016-12-31 19:32:39 +0900

    Trigger Update (미리 만들어 두었다가 나중에 ON)

--- IDCA/Classes/EnemyManager.h
+++ IDCA/Classes/EnemyManager.h
@@ -15,6 +15,7 @@ public :
 	/* Member Variable */
 	CC_SYNTHESIZE(TMXTiledMap*, m_pMap, MapPointer);
 	CC_SYNTHESIZE(bool, m_StageOneTrigger, StageOneTrigger);
+	CC_SYNTHESIZE(int, m_DiedEnemyNum, DiedEnemyNum);
 
 	// 포인터는 원칙적으로 NULL 계산이 필요하기 때문에 NULL이 들어오지 않는 경우에 참조자 반환.
 	Vector<Enemy*>&			getEnemyVector();
@@ -23,6 +24,7 @@ public :
 	
 	/* Member Function */
 	void					MakeEnemy(const ENEMY_TYPE, const Vec2);
+	void					MakeEnemy(const ENEMY_TYPE, const Vec2, const bool);
 	void					ProvidePlayerPosition(const Vec2);
 	Enemy*					FindEnemyWithIdx(const int);
 	Vector<Enemy*>*			FindEnemyWithType(const ENEMY_TYPE);
