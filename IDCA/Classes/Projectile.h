67e1a5c7f58b6e1bf98b15dc54dbe5b3b08f6055
LittleHill\KIM HYUNWOO <massivedynamicny@gmail.com>
2016-12-31 18:56:18 +0900

    스킬 추가중

--- IDCA/Classes/Projectile.h
+++ IDCA/Classes/Projectile.h
@@ -4,7 +4,7 @@
 	class Projectile
 	투사체를 담당하는 기본 클래스.
 	작성일자 : 16.12.01
-	작성자 이근원
+	작성자 김현우
 	투사체 클래스에는 뭐가 필요할까??
 */
 
@@ -13,9 +13,24 @@ class Projectile : public Node
 {
 public :
 	Projectile();
-	virtual ~Projectile() = 0;
+	~Projectile();
 
+	Sprite* GetSprite();
+	Vec2 GetColideRange();
+	int GetDamage();
+
+	void SetStartPosition(Vec2);
+	void SetDirection(Vec2);
+	void SetMaxRange(Vec2);
+	void SetColideRange(Vec2);
+	
+	
 private :
 	Sprite* m_pSprite;
-	int m_UnitVec[2];
+	Vec2 m_startPosition;
+	Vec2 m_DirectionVec;
+	Vec2 m_MaxRange;
+	Vec2 m_ColideRange;
+
+	int m_damage;
 };
---
+++ IDCA/Classes/Skill.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Skill.h"
+
+
+Skill::Skill()
+{
+}
+
+
+Skill::~Skill()
+{
+}
